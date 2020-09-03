bdcf4c67444e14ecee950baba656dfa54dcd507c
kwonhygge <kitteniso1103@gmail.com>
2020-09-04 02:02:22 +0900

    Added signup page

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get('/login', function (req, res) {
   res.render('login');
 });
 
+app.get('/signup', function (req, res) {
+  res.render('signup');
+});
+
 app.get('/create', function (req, res) {
   res.render('create');
 });
